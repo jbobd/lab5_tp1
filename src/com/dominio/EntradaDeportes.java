54b9518f24e2f93be82605175fe2655732c1be6e
Hernan Varela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-03-29 08:35:04 -0300

    29-03-2023 | 8:33
    - Agrego clase EntradaDeporte, que hereda de Entrada
    - Se crean métodos getters y setters
    - Se crean dos consutructores con y sin parámetros.

---
+++ src/com/dominio/EntradaDeportes.java
@@ -0,0 +1,33 @@
+package com.dominio;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class EntradaDeportes extends Entrada {
+	private boolean internacional;
+	private String deporte;
+	
+	/// constructores
+	public EntradaDeportes(){}
+	/// Orden: Internacional, tipo de deporte, nombre de evento, horario, fecha, duración.
+	public EntradaDeportes(boolean internacional, String deporte, String nombre, Time horario, Date fecha, Time duracion){
+		super(nombre, horario, fecha, duracion);
+		this.deporte=deporte;
+		this.internacional=internacional;
+	}	
+	
+	/// getters y setters
+	public boolean isInternacional() {
+		return internacional;
+	}
+	public void setInternacional(boolean internacional) {
+		this.internacional = internacional;
+	}
+	public String getDeporte() {
+		return deporte;
+	}
+	public void setDeporte(String deporte) {
+		this.deporte = deporte;
+	}
+	
+}
