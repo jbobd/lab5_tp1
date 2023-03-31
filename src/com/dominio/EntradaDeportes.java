3511db9bb05e3b16b318309db7eea81164149ec9
Hernan Varela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-03-31 07:56:56 -0300

    31-03-2023 | 7:52
    - Agrego enum ETipoDeporte y lo aplico en clase EntradaDeportes
    - Modifico método calcularValor() y toString() para aplicar el enum
    - Condicional ternario para mejorar el texto de salida.

--- src/com/dominio/EntradaDeportes.java
+++ src/com/dominio/EntradaDeportes.java
@@ -3,14 +3,16 @@ package com.dominio;
 import java.sql.Date;
 import java.sql.Time;
 
+import com.dominio.enums.ETipoDeporte;
+
 public class EntradaDeportes extends Entrada {
 	private boolean internacional;
-	private String deporte;
+	private ETipoDeporte deporte;
 	
 	/// constructores
 	public EntradaDeportes(){}
 	/// Orden: Internacional, tipo de deporte, nombre de evento, horario, fecha, duración.
-	public EntradaDeportes(boolean internacional, String deporte, String nombre, Time horario, Date fecha, Time duracion){
+	public EntradaDeportes(boolean internacional, ETipoDeporte deporte, String nombre, Time horario, Date fecha, Time duracion){
 		super(nombre, horario, fecha, duracion);
 		this.deporte=deporte;
 		this.internacional=internacional;
@@ -22,26 +24,26 @@ public class EntradaDeportes extends Entrada {
 	public void setInternacional(boolean internacional) {
 		this.internacional = internacional;
 	}
-	public String getDeporte() {
+	public ETipoDeporte getDeporte() {
 		return deporte;
 	}
-	public void setDeporte(String deporte) {
+	public void setDeporte(ETipoDeporte deporte) {
 		this.deporte = deporte;
 	}
 	/// Calcula valor de la entrada en función del deporte y el tipo de evento
 	@Override
 	public void calcularValor() {
 		this.setValor(0);
-		if(deporte.equalsIgnoreCase("futbol")){this.setValor(300);}
-		if(deporte.equalsIgnoreCase("rugby")){this.setValor(450);}
-		if(deporte.equalsIgnoreCase("hockey")){this.setValor(380);}
+		if(deporte.name().equalsIgnoreCase(ETipoDeporte.FUTBOL.name())){this.setValor(300);}
+		if(deporte.name().equalsIgnoreCase(ETipoDeporte.RUGBY.name())){this.setValor(450);}
+		if(deporte.name().equalsIgnoreCase(ETipoDeporte.HOCKEY.name())){this.setValor(380);}
 		if(internacional) {this.setValor(getValor()*1.3);}
 	}
 	/// Metodo toString() con llamado al mismo método de Entrada
 	@Override
 	public String toString() {
 		return super.toString() +
-			   "Es internacional: " + internacional + "\n" +
-			   "Deporte: " + deporte + "\n";
+			   "Es internacional: " + (internacional ? "Si" : "No") + "\n" +
+			   "Deporte: " + deporte.name() + "\n";
 	}
 }
---
+++ src/com/dominio/enums/ETipoDeporte.java
@@ -0,0 +1,5 @@
+package com.dominio.enums;
+
+public enum ETipoDeporte {
+	FUTBOL, RUGBY, HOCKEY
+}
